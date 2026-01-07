d31e76d54eeaaf1b7d78a252a0ea6feaf24c8b66
Damiano Oldoni <damiano.oldoni@inbo.be>
2026-01-07 16:03:28 +0100

    Improve leaflet heatmap and save screenshot

--- src/heatmap_occurrences.R
+++ src/heatmap_occurrences.R
@@ -6,7 +6,8 @@ library(readr)
 library(dplyr)
 library(here)
 library(leaflet)
-library(webshot)
+library(htmlwidgets)
+library(webshot2)
 
 
 # Download and load GBIF occurrence data
@@ -25,34 +26,51 @@ occ_path <- file.path(exdir, occ_file)
 occs <- readr::read_tsv(occ_path, guess_max = 100000, show_col_types = FALSE)
 
 # Create a leaflet heatmap of occurrences based on columns "decimalLongitude" and "decimalLatitude".
-# The heatmap should refer to the number of occurrences in each area, i.e. number of rows with similar coordinates
+# The heatmap should refer to the number of occurrences in each area, i.e. number of rows with similar coordinates.
+# Add a layer with occurrences as circles. This layer should be toggleable on/off. Default: off.
+# Add base tiles: gray, topo, streetmap, hybrid, satellites.
 heatmap_occs <- leaflet::leaflet(data = occs) %>%
   leaflet::setView(lng = 4.5, lat = 50.5, zoom = 8) %>%
-  leaflet::addTiles() %>%
+  leaflet::addProviderTiles("CartoDB.Positron", group = "Gray") %>%
+  leaflet::addProviderTiles("OpenTopoMap", group = "Topo") %>%
+  leaflet::addProviderTiles("OpenStreetMap.Mapnik", group = "Streetmap") %>%
+  leaflet::addProviderTiles("Esri.WorldImagery", group = "Satellite") %>%
+  leaflet::addProviderTiles("Esri.WorldImagery", group = "Hybrid") %>%
   leaflet.extras::addHeatmap(
     lng = ~decimalLongitude,
     lat = ~decimalLatitude,
-    intensity = ~0.8,
+    intensity = ~1,
     blur = 20,
     max = 0.05,
     radius = 15
   ) %>%
-  leaflet::addLegend(
-    position = "bottomleft",
-    title = "Dichtheid van waarnemingen",
-    colors = c("red", "yellow", "green", "blue"),
-    labels = c("Hoog", "", "", "Laag")
-  )
+  leaflet::addCircleMarkers(
+    lng = ~decimalLongitude,
+    lat = ~decimalLatitude,
+    radius = 1,
+    color = "black",
+    fillOpacity = 0.5,
+    group = "Occurrences"
+  ) %>%
+  leaflet::addLayersControl(
+    baseGroups = c("Gray", "Topo", "Streetmap", "Satellite", "Hybrid"),
+    overlayGroups = c("Occurrences"),
+    options = leaflet::layersControlOptions(collapsed = TRUE)
+  ) %>%
+  leaflet::hideGroup("Occurrences")
 heatmap_occs
+
 # Save heatmap as HTML
 htmlwidgets::saveWidget(
   heatmap_occs,
   here::here("figures", "heatmap_occurrences.html")
 )
 
-# Save heatmap as PNG
-mapview::mapshot(
-  heatmap_occs,
+# Save screenshot as PNG using webshot2
+webshot2::webshot(
+  url = here::here("figures", "heatmap_occurrences.html"),
   file = here::here("figures", "heatmap_occurrences.png"),
-  remove_controls = "zoomControl"
+  vwidth = 1000,
+  vheight = 800,
+  delay = 2
 )
